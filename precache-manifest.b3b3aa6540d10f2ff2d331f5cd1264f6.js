1c9b724a4b77dbf7726fce0137ace7aad5b03fc0
Vince Charals <vincecharals@gmail.com>
2019-04-27 11:54:12 -0500

    Updates

---
+++ precache-manifest.b3b3aa6540d10f2ff2d331f5cd1264f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f1cd4eecbad944840613559b2672bc",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "cc87a65e87a2b6402fcb",
+    "url": "/Clicky-Game/static/css/main.a84069b1.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "cc87a65e87a2b6402fcb",
+    "url": "/Clicky-Game/static/js/main.40e30228.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
