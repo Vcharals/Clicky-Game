0b696680840f3c11eec984d339e1ffb82b7c7356
Vince Charals <vincecharals@gmail.com>
2019-04-27 11:46:53 -0500

    gh-pages tweak

---
+++ build/precache-manifest.9f327efc56b2ba0edaadf9d760290507.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea3f1310706cd3e639835fccc0039ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee884afc145f2d693a4",
+    "url": "/static/css/main.a84069b1.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "aee884afc145f2d693a4",
+    "url": "/static/js/main.ed4e8a04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
